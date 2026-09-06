b281d1bb041f55bc94cc548afc3e2127fedb92cf
Masum75-max <almasumabdullah414@gmail.com>
2026-09-07 01:33:20 +0600

    amar accepted code in geek for geeks

---
+++ Arrays/SecondLargest.cpp
@@ -0,0 +1,14 @@
+class Solution {
+  public:
+    int getSecondLargest(vector<int> &arr) {
+        // code here
+       int n= arr.size();
+       
+       sort(arr.begin(),arr.end());
+       
+       for(int i =n-2;i>=0;i--){
+           if(arr[i]!=arr[i+1]) return arr[i];
+       }
+       return -1;
+    }
+};
