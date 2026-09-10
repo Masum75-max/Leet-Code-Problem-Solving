ec7956d9713eba8c13e7d86782f228e8f5963a43
Masum75-max <almasumabdullah414@gmail.com>
2026-09-11 01:39:39 +0600

    rotate array by one element

---
+++ Arrays/rotateeArrayByOneElement.cpp
@@ -0,0 +1,11 @@
+class Solution {
+  public:
+    void rotate(vector<int> &arr) {
+        // code here
+        int n = arr.size();
+        
+        for (int i=0;i<n;i++){
+            swap(arr[i],arr[n-1]);
+        }
+    }
+};
