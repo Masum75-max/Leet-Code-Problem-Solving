69e10fe49f005850612b04042169da8110054dc2
Masum75-max <almasumabdullah414@gmail.com>
2026-09-18 14:43:55 +0600

    added missing element leetcode problem

---
+++ Arrays/missingElementsLeetCode.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+
+        int n= nums.size();
+
+        int sum = n*(n+1)/2;
+
+        int loopSum =0;
+
+        for(int i=0;i<n;i++){
+            loopSum+= nums[i];
+        }
+        
+        return sum-loopSum;
+    }
+};
